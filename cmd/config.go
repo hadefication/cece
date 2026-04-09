fbce79c0146d5450b2fab3ac18eb970b24f2530c
Glen Bangkila <hadeinvane@gmail.com>
2026-04-09 11:56:32 +0800

    feat: add config show and config path commands

---
+++ cmd/config.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/inggo/cece/internal/config"
+	"github.com/spf13/cobra"
+)
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage cece configuration",
+}
+
+var configShowCmd = &cobra.Command{
+	Use:   "show",
+	Short: "Print current configuration",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		data, err := os.ReadFile(config.FilePath())
+		if err != nil {
+			if os.IsNotExist(err) {
+				return fmt.Errorf("cece is not initialized. Run: cc init")
+			}
+			return err
+		}
+		fmt.Print(string(data))
+		return nil
+	},
+}
+
+var configPathCmd = &cobra.Command{
+	Use:   "path",
+	Short: "Print config directory path",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.Load()
+		if err != nil {
+			return err
+		}
+
+		dir, err := cfg.ResolveProfileDir(profile)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(dir)
+		return nil
+	},
+}
+
+func init() {
+	configCmd.AddCommand(configShowCmd)
+	configCmd.AddCommand(configPathCmd)
+	rootCmd.AddCommand(configCmd)
+}
